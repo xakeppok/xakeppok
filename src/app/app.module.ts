bc2ce86bb671f860bcb06d19d39fd83fa016c558
xakeppok <xakeppok10@gmail.com>
2019-04-04 22:45:34 +0300

    verstka rdy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GoodsComponent } from './goods/goods.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GoodsComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
